9af76c7ff31c6bd9f639f5ed5f838bab4f013363
kai.beisheim <kai.w.beisheim@gmail.com>
2023-10-09 16:39:01 +0200

    Fixed "cloud" block for GitHub Action (Plan/Apply)

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.5.1"
     }
     azurerm = {
@@ -9,11 +9,11 @@ terraform {
       version = "3.75.0"
     }
     ionoscloud = {
-      source = "ionos-cloud/ionoscloud"
+      source  = "ionos-cloud/ionoscloud"
       version = "6.4.9"
     }
     github = {
-      source = "integrations/github"
+      source  = "integrations/github"
       version = "5.39.0"
     }
   }
