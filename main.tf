036e888e4296fcb14afe257f936ea57230b9e8db
kai.beisheim <kai.w.beisheim@gmail.com>
2024-03-04 10:16:27 +0100

    Added Cloudflare DNS

--- main.tf
+++ main.tf
@@ -4,17 +4,30 @@ terraform {
       source  = "hashicorp/random"
       version = "3.6.0"
     }
+
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "3.94.0"
     }
-    ionoscloud = {
-      source  = "ionos-cloud/ionoscloud"
-      version = "6.4.13"
+
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "4.25.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+
+  skip_provider_registration = "true"
+
+  tenant_id       = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
 }
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "azure_tenant_id" {
+  type = string
+}
+
+variable "azure_subscription_id" {
+  type = string
+}
+
+variable "azure_client_id" {
+  type = string
+}
+
+variable "azure_client_secret" {
+  type = string
+}
+
+variable "cloudflare_api_token" {
+  type = string
+}
+
+variable "apple_domain" {
+  type = string
+}
